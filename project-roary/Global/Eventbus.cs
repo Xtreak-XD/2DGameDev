d8969b5dc5d8a618bed4c141300fb2fa6369e4c2
CR1SI <cristianpmason@icloud.com>
2025-10-07 15:05:32 -0400

    projectSetup

---
+++ project-roary/Global/Eventbus.cs
@@ -0,0 +1,10 @@
+using Godot;
+using System;
+
+/// <summary>
+/// Eventbus class for all events in the game.
+/// Please document events here with a description of what they do and where they are used!
+/// </summary>
+public partial class Eventbus : Node
+{
+}

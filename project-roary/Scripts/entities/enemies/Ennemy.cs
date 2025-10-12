c14160f675e9a1c1382a8e233cc9d229b927d41b
Ill-Come-Up-With-The-Name-Later <adtmax5@gmail.com>
2025-10-12 04:05:39 -0400

    Placeholder Logic For Enemy.cs
    Enemy should now have a way to be "damaged" and "die"

--- project-roary/Scripts/entities/enemies/Ennemy.cs
+++ project-roary/Scripts/entities/enemies/Ennemy.cs
@@ -3,6 +3,11 @@ using Godot;
 public partial class Ennemy : CharacterBody2D
 {
 	const int speed = 100;
+	const double attackRange = 10.0;
+	const int damage = 1;
+	public int Health { get; private set; } = 5;
+
+	//public Player Target {get; set; }; USE THIS LATER
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -12,10 +17,30 @@ public partial class Ennemy : CharacterBody2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
     {
-        Vector2 targetPos = GetGlobalMousePosition();
+		Vector2 targetPos = GetGlobalMousePosition();
+		//Vector2 targetPos = Target.GlobalPosition; // USE THIS LATER INSTEAD OF MOUSE POS
 		Vector2 direction = (targetPos - GlobalPosition).Normalized();
 
 		Velocity = direction * speed;
 		MoveAndSlide();
-    }
+
+		if(Position.DistanceTo(targetPos) < attackRange)
+		{
+			GD.Print("Attack for " + damage + " damage.");
+		}
+	}
+
+	public void TakeDamage(int amount)
+	{
+		Health -= amount;
+		if(Health <= 0)
+		{
+			Die();
+		}
+	}
+	
+	public void Die()
+	{
+		QueueFree();
+	}
 }

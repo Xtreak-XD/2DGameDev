8e1075be2301c7d89152d736f38d42cef681b1d5
Cristian <cristianpmason@icloud.com>
2025-10-19 19:12:31 -0400

    not needed

--- project-roary/Scripts/ITakeDamage.cs
+++
@@ -1,8 +0,0 @@
-using Godot;
-using System;
-
-public interface ITakeDamage
-{
-    void TakeDamage(float damage, Vector2? attackFromVector); //Attack from vector could be changed to the correct variable name created from the hitbox
-    
-}

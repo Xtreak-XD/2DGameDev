bf02914823ab0109d9c3d6e4488d5b204ae63b3e
Cristian <cristianpmason@icloud.com>
2025-11-01 11:36:13 -0400

    ADDED feature
    Added day night cycle controller
    Correctly used a sin function to imitate day/night and a color palette for the colors.
    Connected to the Ui so it correctly changes the time, but still not connected to actual day just time so far.
    
    Created event that passes day, hour, and min so anyone can call it if they need the day and/or time

---
+++ project-roary/Scripts/dayNight/DayNightCycle.cs
@@ -0,0 +1,67 @@
+using Godot;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+public partial class DayNightCycle : CanvasModulate
+{
+    public double time = 0.0;
+    public double pastMin = -1.0;
+    const int MINUTES_PER_DAY = 1440;
+    const int MIUNTES_PER_HOUR = 60;
+    const float INGAME_TO_REAL_MINUTE_DURATION = (2 * Mathf.Pi) / MINUTES_PER_DAY;
+
+    public Eventbus eventbus;
+
+    [Export]
+    public GradientTexture1D gradient;
+    [Export]
+    public double INGAME_SPEED = 1.0;
+    private int initialHour = 12;
+    [Export]
+    public int INITIAL_HOUR
+    {
+        get { return initialHour; }
+        set
+        {
+            if (value > 24 || value < 0)
+            {
+                return;
+            }
+            initialHour = value;
+        }
+        
+    }
+
+    public override void _Ready()
+    {
+        time = INGAME_TO_REAL_MINUTE_DURATION * INITIAL_HOUR * MIUNTES_PER_HOUR;
+    }
+
+    public override void _Process(double delta)
+    {
+        eventbus = GetNode<Eventbus>("/root/Eventbus");
+        time += delta * INGAME_SPEED * INGAME_TO_REAL_MINUTE_DURATION;
+        double value = (Mathf.Sin(time - Mathf.Pi) + 1.0) / 2.0;
+        Color = gradient.Gradient.Sample((float)value);
+
+        _recalculateTime();
+    }
+
+    public void _recalculateTime()
+    {
+        int total_mins = (int)(time / INGAME_TO_REAL_MINUTE_DURATION);
+
+        int day = (int)(total_mins / MINUTES_PER_DAY);
+        int currentDayMins = total_mins % MINUTES_PER_DAY;
+        var hour = (int)(currentDayMins / MIUNTES_PER_HOUR);
+        var mins = (int)(currentDayMins % MIUNTES_PER_HOUR);
+
+        if (pastMin != mins)
+        {
+            pastMin = mins;
+            eventbus.EmitSignal("timeTick", day, hour, mins);
+        }
+        
+    }
+
+}
--- project-roary/Scripts/ui/Interface.cs
+++ project-roary/Scripts/ui/Interface.cs
@@ -1,7 +1,37 @@
 using Godot;
 using System;
 
+
 public partial class Interface : CanvasLayer
 {
+    public Eventbus eventbus;
+    public Label time;
+    public Label day;
+    public Label temp;
+
+    public override void _Ready()
+    {
+        eventbus = GetNode<Eventbus>("/root/Eventbus");
+        eventbus.timeTick += setTime;
+
+        time = GetNode<Label>("HUD/day cycle/PanelContainer/cycleInfo/time");
+        day = GetNode<Label>("HUD/day cycle/PanelContainer/cycleInfo/day");
+        temp = GetNode<Label>("HUD/day cycle/PanelContainer/cycleInfo/temp");
+    }
+    
+
+    public void setTime(int day, int hour, int min)
+    {
+        if (hour >= 12 && hour <= 23)
+        {
+            time.Text = hour.ToString() + ":" + min.ToString() + " PM";
+        }
+        else
+        {
+            time.Text = hour.ToString() + ":" + min.ToString() + " AM";
+        }
+    }
+
     //use this to receive events for player input related to pause/and ui
+
 }

38215ef8d05149a0d7a6169d92cc0a1c06af2f50
avuto001 <vutovalex@gmail.com>
2025-11-06 15:29:51 -0500

    Car Fix

--- project-roary/Scripts/entities/enemies/CarEnemy.cs
+++ project-roary/Scripts/entities/enemies/CarEnemy.cs
@@ -24,27 +24,19 @@ public partial class CarEnemy : Enemy
         {
             case directionChosen.North:
                 Velocity = Vector2.Up * speed * (float)delta;
-                GetNode<Sprite2D>("Sprite2D").RotationDegrees = 0;
-                GetNode<Area2D>("Hitbox").RotationDegrees = 0;
-                GetNode<CollisionShape2D>("CollisionShape2D").RotationDegrees = 0;
+                RotationDegrees = 270;
                 break;
             case directionChosen.East:
                 Velocity = Vector2.Right * speed * (float)delta;
-                GetNode<Sprite2D>("Sprite2D").RotationDegrees = 90;
-                GetNode<Area2D>("Hitbox").RotationDegrees = 90;
-                GetNode<CollisionShape2D>("CollisionShape2D").RotationDegrees = 90;
+                RotationDegrees = 0;
                 break;
             case directionChosen.South:
                 Velocity = Vector2.Down * speed * (float)delta;
-                GetNode<Sprite2D>("Sprite2D").RotationDegrees = 180;
-                GetNode<Area2D>("Hitbox").RotationDegrees = 180;
-                GetNode<CollisionShape2D>("CollisionShape2D").RotationDegrees = 180;
+                RotationDegrees = 90;
                 break;
             case directionChosen.West:
                 Velocity = Vector2.Left * speed * (float)delta;
-                GetNode<Sprite2D>("Sprite2D").RotationDegrees = 270;
-                GetNode<Area2D>("Hitbox").RotationDegrees = 270;
-                GetNode<CollisionShape2D>("CollisionShape2D").RotationDegrees = 270;
+                RotationDegrees = 180;
                 break;
         }
     }

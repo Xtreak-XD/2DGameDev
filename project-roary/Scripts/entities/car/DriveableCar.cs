810809e8007f9e3aeab39fea3ac9b1089154374e
Ill-Come-Up-With-The-Name-Later <magidonovan@gmail.com>
2025-11-10 13:54:03 -0500

    Functional Car
    The car is now functionally completed. It rotates and moves correctly.

--- project-roary/Scripts/entities/car/DriveableCar.cs
+++ project-roary/Scripts/entities/car/DriveableCar.cs
@@ -4,10 +4,10 @@ using Godot;
 public partial class DriveableCar : CharacterBody2D
 {
 	public CarStateMachine stateMachine;
-	public Vector2 direction = Vector2.Zero;
 
 	[Export]
 	public CarStats stats;
+	public Vector2 direction = Vector2.Zero;
 
 	public override void _Ready()
 	{
@@ -19,64 +19,49 @@ public partial class DriveableCar : CharacterBody2D
 
 	public override void _Process(double delta)
 	{
-		// TODO: FIGURE OUT HOW TO SMOOTH OUT THE MOVEMENT INPUT AND ROTATION
-		float upInput = Input.GetActionStrength("Up");
-		float leftInput = Input.GetActionStrength("Left");
-		float rightInput = Input.GetActionStrength("Right");
-		int downInput = (int)Input.GetActionStrength("Down");
-
-		direction.X += upInput;
-		direction.X = Mathf.Clamp(direction.X, -1, 1);
-
-		if (ShouldReverse())
-		{
-			direction.X *= -1;
-		}
-
-		if (!(stateMachine.currentState is ParkState))
-		{
-			if (rightInput != 0 || leftInput != 0)
-			{
-				Rotation += Mathf.DegToRad((rightInput - leftInput) * stats.SteeringSpeed * (float)delta);
-				direction += Vector2.Right.Rotated(Rotation);
-			}
-		}
-		
-		direction = direction.Normalized();
 	}
 
-	public bool ShouldReverse()
+	public override void _PhysicsProcess(double delta)
     {
-		int downActionStrength = (int)Input.GetActionStrength("Down");
-		int upActionStrength = (int)Input.GetActionStrength("Up");
-		return downActionStrength != 0 && upActionStrength == 0;
+		float turn = Input.GetAxis("Left", "Right");
+		float turnAngle = Mathf.DegToRad(turn * stats.SteeringSpeed);
+		Vector2 accelerationVector = Vector2.Zero;
+
+		if (Input.IsActionPressed("Up"))
+		{
+			accelerationVector = Transform.X * stats.Acceleration;
+		}
+
+		if (Input.IsActionPressed("Down"))
+		{
+			accelerationVector = Transform.X * (-stats.Acceleration * 2);
+		}
+
+		Velocity += accelerationVector * (float)delta;
+		Velocity = Velocity.Rotated(turnAngle);
+
+		SetRotation();
+		MoveAndSlide();
     }
 
 	public bool HasThrottle()
-	{
-		/*
-		int rightActionStrength = (int)Input.GetActionStrength("Right");
-		int leftActionStrength = (int)Input.GetActionStrength("Left");
-		int upActionStrength = (int)Input.GetActionStrength("Up");
-		int downActionStrength = (int)Input.GetActionStrength("Down");
-
-		return rightActionStrength != 0 ||
-			   leftActionStrength != 0 ||
-			   upActionStrength != 0 ||
-			   downActionStrength != 0;
-			   */
-
-		int upActionStrength = (int)Input.GetActionStrength("Up");
-		int downActionStrength = (int)Input.GetActionStrength("Down");
-		return upActionStrength != 0 || downActionStrength != 0;
+	{	
+		return Input.IsActionPressed("Up");
 	}
 
 	public void SetRotation()
 	{
-		// NOT SURE IF THIS IS THE BEST WAY TO DO IT
-		Rotation = direction.Angle();
-    }
-	
+		if (stateMachine.currentState is ParkState)
+		{
+			return;
+		}
+		
+		if (Velocity != Vector2.Zero)
+		{
+			Rotation = Velocity.Angle();
+		}
+	}
+
 	public bool IsParked()
 	{
 		return Velocity.Length() == 0;
--- project-roary/Scripts/entities/car/car_state_machine/DriveState.cs
+++ project-roary/Scripts/entities/car/car_state_machine/DriveState.cs
@@ -24,26 +24,24 @@ public partial class DriveState : CarState
         {
             return DecelerateState;
         }
-        
+
         GD.Print($"Car Velocity: ({ActiveCar.Velocity.X}," +
         $"{ActiveCar.Velocity.Y})");
         GD.Print("Car Speed: " + ActiveCar.GetVelocity().Length());
+        GD.Print("Car Position: " + ActiveCar.Position);
+        
+        /*
+        ActiveCar.Velocity = ActiveCar.Velocity.Lerp(ActiveCar.direction
+         * ActiveCar.stats.Acceleration * (float)delta,
+        ActiveCar.stats.TopSpeed);
+        */
 
-        if (ActiveCar.Velocity.Length() < ActiveCar.stats.TopSpeed)
+        if (ActiveCar.Velocity.Length() > ActiveCar.stats.TopSpeed)
         {
-            ActiveCar.Velocity
-            += ActiveCar.direction *
-            ActiveCar.stats.Acceleration * (float)delta;
-
-            if(ActiveCar.Velocity.Length() > ActiveCar.stats.TopSpeed)
-            {
-                ActiveCar.Velocity = ActiveCar.Velocity.Normalized()
-                 * ActiveCar.stats.TopSpeed;
-            }
+            ActiveCar.Velocity = ActiveCar.Velocity.Normalized()
+             * ActiveCar.stats.TopSpeed;
         }
 
-        ActiveCar.SetRotation();
-        ActiveCar.MoveAndCollide(ActiveCar.Velocity);
         return null;
     }
 }

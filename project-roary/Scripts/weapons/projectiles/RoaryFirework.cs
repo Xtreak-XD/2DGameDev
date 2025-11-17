c805322726b257f37cab6c52c5d516c54da7ea40
Ill-Come-Up-With-The-Name-Later <magidonovan@gmail.com>
2025-11-17 17:18:07 -0500

    Firework Behavior
    Made Roary's firework's behavior

--- project-roary/Scripts/weapons/projectiles/RoaryFirework.cs
+++ project-roary/Scripts/weapons/projectiles/RoaryFirework.cs
@@ -4,7 +4,7 @@ using Godot;
 public partial class RoaryFirework : EnemyProjectile
 {
 	public Player target;
-	public const float HOMING_RANGE = 550f;
+	public const float HOMING_RANGE = 750f;
 
     public override void Travel(double delta)
     {
@@ -20,9 +20,9 @@ public partial class RoaryFirework : EnemyProjectile
 			if(currentPos.DistanceTo(targetPos) <= HOMING_RANGE)
             {
 				float angle = currentPos.AngleTo(targetPos);
-                Vector2 velocity = new Vector2(Mathf.Cos(angle), MathF.Sin(angle)).Normalized();
+                Vector2 velocity = Velocity + new Vector2(Mathf.Cos(angle), MathF.Sin(angle) * data.speed);
 
-				Velocity = velocity * data.speed;
+				Velocity = velocity;
             }
         }
 

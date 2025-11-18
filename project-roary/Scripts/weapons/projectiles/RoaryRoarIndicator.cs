18a15a986fc59a77d850f56977c5c95ba1f11f79
Ill-Come-Up-With-The-Name-Later <magidonovan@gmail.com>
2025-11-18 05:58:37 -0500

    Roary Roar
    Roary's thunderous roar now expands properly.

--- project-roary/Scripts/weapons/projectiles/RoaryRoarIndicator.cs
+++ project-roary/Scripts/weapons/projectiles/RoaryRoarIndicator.cs
@@ -1,10 +1,17 @@
+using Godot;
+
 public partial class RoaryRoarIndicator : EnemyProjectile
 {
     public override void Travel(double delta)
     {   
-        sprite.Scale *= 1.05f;
-        hitbox.Scale *= 1.05f;
+        Scale += new Vector2(25f * (float) delta, 25f * (float) delta);
+    }
 
-        base.Travel(delta);
+    public override void HitEntity(Area2D area)
+    {
+        if (area.GetParent().IsInGroup("player"))
+        {
+            GD.Print("Player has been hit by Roary's roar");
+        }
     }
 }

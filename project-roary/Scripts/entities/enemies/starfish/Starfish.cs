33e758a857b6f6d97c3c868b24347bac83aca3b7
Tactfulmedia-84 <aogaliber3@gmail.com>
2025-11-21 13:27:02 -0500

    Feature/starfish (#118)
    * Starfish draft
    
    *Set up scenes for Starfish enemy and Bubble attack
    *Implemented placeholder sprites
    *Created foundation of behavior scripts for Starfish
    
    * Starfish Bubble Shooting
    
    The starfish now shoots a bubble at the player periodically.
    
    * Bubble Homing
    
    The starfish's bubble now homes in on the player.
    
    * Fixes to Starfish Parent and Bubble projectile
    
    * Bubble is rooted from projectilecontainer, now independent from Starfish
    
    *Starfish has checks on when target is null
    
    *ExitTree now integrated for Starfish
    
    *Starfish now is inherited in the proper class
    
    *Fixed small errors regarding timer
    
    * ExitTree and Independence of Projectile
    
    Unsubscribed from timer timeouts in ExitTree and the starfish's projectile should be independent of it.
    
    * Changes to Parent and Variables in Script
    
    *Replaced Starfish as owner to ProjectileContainer
    
    *Removed lines of code containing the "hitbox" Variable
    
    *Reorganized the SceneTree variables
    
    * Fixed Test Scene
    
    Fixed the test scene not working.
    
    ---------
    
    Co-authored-by: Ill-Come-Up-With-The-Name-Later <magidonovan@gmail.com>
    Co-authored-by: Ill-Come-Up-With-The-Name-Later <117209235+Ill-Come-Up-With-The-Name-Later@users.noreply.github.com>

---
+++ project-roary/Scripts/entities/enemies/starfish/Starfish.cs
@@ -0,0 +1,67 @@
+using System;
+using Godot;
+
+public partial class Starfish : Enemy
+{
+	[Export] 
+
+	//SceneTree Variables
+	public Player target;
+	public Area2D hurtBox;
+	public Marker2D projectileSource;
+	public Timer setTargetTimer;
+
+	[Export] public PackedScene EnemyScene; // Assign your enemy scene in the Inspector
+    [Export] public Vector2 AreaSize = new Vector2(500, 300); // Width & height of spawn area
+    [Export] public Vector2 AreaCenter = Vector2.Zero; // Center of spawn area
+
+	[Export]
+	public PackedScene bubbleProjectile;
+
+	[Export]
+	public Timer projectileTimer;
+
+	public override void _Ready()
+    {
+		hurtBox = GetNode<Area2D>("HurtBox");
+		projectileSource = GetNode<Marker2D>("ProjectileSource");
+		setTargetTimer = GetNode<Timer>("SetTargetTimer");
+
+		projectileTimer.Timeout += ShootBubble;
+		setTargetTimer.Timeout += SetTarget;
+		setTargetTimer.Start();
+    }
+    
+
+    public override void _EnterTree()
+    {
+        AddToGroup("enemy");
+    }
+
+    public override void _ExitTree()
+    {
+        projectileTimer.Timeout -= ShootBubble;
+		setTargetTimer.Timeout -= SetTarget;
+    }
+
+	public void SetTarget()
+    {
+        target = (Player)GetTree().GetFirstNodeInGroup("player");
+    }
+
+	// Aim projectile
+	private void ShootBubble()
+    {
+		Vector2 currentPos = projectileSource.GlobalPosition;
+		Vector2 targetPos = target.GlobalPosition;
+
+		StarfishBubble bubble = (StarfishBubble)bubbleProjectile.Instantiate();
+		GetNode("ProjectileContainer").AddChild(bubble);
+
+		bubble.GlobalPosition = currentPos;
+		bubble.target = target;
+
+		Vector2 direction = (targetPos - currentPos).Normalized();
+		bubble.Velocity = direction * bubble.data.speed;
+    }
+}
